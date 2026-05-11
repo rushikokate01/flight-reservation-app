50e8153ea050051297d6697076725e5c23bcabeb
rushikokate01 <rushikokate15@gmail.com>
2026-05-12 00:55:31 +0530

    update docker image

--- FlightReservationApplication/backend-app.groovy
+++ FlightReservationApplication/backend-app.groovy
@@ -3,7 +3,7 @@ pipeline {
     stages {
         stage('Code-Pull'){
             steps{
-                git branch: 'main', url: 'https://github.com/mayurmwagh/flight-reservation-app.git'    
+                git branch: 'main', url: 'https://github.com/rushikokate01/flight-reservation-app.git'    
             }
         }
         stage('Code-Build'){
